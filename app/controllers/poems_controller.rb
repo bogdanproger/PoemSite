aab6f8993bb667b2322bd3629a70cb57ebe620fa
bogdanproger <bogdanrymar39@gmail.com>
2022-04-25 11:17:37 +0300

    CRUD: new

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,4 +2,7 @@ class PoemsController < ApplicationController
 def index
   @poems = Poem.all
 end
+def new
+@poems = Poem.new
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get '/poems', to: 'poems#index'
+  get '/poems/new', to: 'poems#new'
 end
