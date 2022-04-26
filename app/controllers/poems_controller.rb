ef6fd9109264a9372a33a3fb0e4b7f269489e3ad
bogdanproger <bogdanrymar39@gmail.com>
2022-04-26 23:39:52 +0300

    Update

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,23 +1,34 @@
 class PoemsController < ApplicationController
+
+  def edit
+    @poem = Poem.find_by id: params[:id]
+  end
+
+  def update
+    @poem = Poem.find_by id: params[:id]
+    if @poem.update poem_params
+      redirect_to poems_path
+    else render :edit
+    end
+  end
+
+
 def index
   @poems = Poem.all
 end
 
 def new
-  @poems = Poem.new
+  @poem = Poem.new
 end
 
 def create
-  @poems = Poem.new poem_params
-  if @poems.save
+  @poem = Poem.new poem_params
+  if @poem.save
     redirect_to poems_path
   else render :new
   end
 end
 
-def edit
-  @poem = Poem.find_by id: params[:id]
-end
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
+  resources :poems, only: %i[index new edit create update]
   root 'pages#index'
-  #get '/poems', to: 'poems#index'
-  #get '/poems/new', to: 'poems#new'
-  #get '/poems/:id/edit', to: 'poems#edit'
-  post '/poems', to: 'poems#create'
-
-  resources :poems, only: %i[index new edit create]
 end
