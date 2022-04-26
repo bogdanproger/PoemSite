1fed888c900201aaadb4e1c9095a2cbf8ab31287
bogdanproger <bogdanrymar39@gmail.com>
2022-04-26 22:14:05 +0300

    Create

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,7 +2,22 @@ class PoemsController < ApplicationController
 def index
   @poems = Poem.all
 end
+
 def new
-@poems = Poem.new
+  @poems = Poem.new
+end
+
+def create
+  @poems = Poem.new poem_params
+  if @poems.save
+    redirect_to poems_path
+  else render :new
+  end
+end
+
+private
+
+def poem_params
+  params.require(:poem).permit(:title, :body)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '/poems', to: 'poems#index'
   get '/poems/new', to: 'poems#new'
+  post '/poems', to: 'poems#create'
 end
