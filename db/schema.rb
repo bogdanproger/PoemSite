6179b8135e03450c023657d220c186c38f0dda98
bogdanproger <bogdanrymar39@gmail.com>
2022-05-02 21:21:54 +0300

    Seeds, fakers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_02_121213) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
