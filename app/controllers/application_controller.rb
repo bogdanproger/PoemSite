dd7d37548fab3660f606a6a4472f1500a805e672
bogdanproger <bogdanrymar39@gmail.com>
2022-05-04 07:53:02 +0300

    Edit views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
 rescue_from ActiveRecord::RecordNotFound, with: :notfound
 
 private
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+end
+
+def user_signed_in?
+  current_user.present?
+end
+
+helper_method :current_user, :user_signed_in?
+
+
 def notfound(exception)
   logger.warn exception
 render file: 'public/404.html', status: :not_found, layout: false
